d5001f4404748e6efe45b6e82054fb9ef391efd7
Fudanyrd <143480204+Fudanyrd@users.noreply.github.com>
2024-09-04 17:42:01 +0800

    [IMPL] booting

--- src/main.c
+++ src/main.c
@@ -3,19 +3,49 @@
 #include <kernel/printk.h>
 #include <kernel/core.h>
 #include <common/string.h>
+#include <common/debug.h>
 
 static volatile bool boot_secondary_cpus = false;
 
+#define HELLO(core) do { \
+		printk("Hello world! (Core %d)\n", core); \
+	} while(0)
+
+#ifdef ASSERT
+#undef ASSERT
+// assertion
+#define ASSERT(cond) \
+    do { \
+      if (!(cond)) { \
+        printk("Assertion failure: " #cond  \
+            " at [File %s] [Function %s] [Line %d]" \
+            , __FILE__, __FUNCTION__, __LINE__); \
+        for (;;) {} \
+      }\
+    } while(0)
+#endif
+
 void main()
 {
+	/* edata is .bss start addr */
+	extern char edata[];
+	/* end is .bss end addr */
+	extern char end[];
+	ASSERT (end != NULL && edata != NULL);
+	ASSERT (end >= edata);
+
 	if (cpuid() == 0) {
 		/* @todo: Clear BSS section.*/
+		memset(edata, 0, (usize)(end - edata));
+		/* @end todo */
 
 		smp_init();
 		uart_init();
 		printk_init();
 
 		/* @todo: Print "Hello, world! (Core 0)" */
+		HELLO(0);
+		/* @end todo */
 
 		arch_fence();
 
@@ -27,6 +57,8 @@ void main()
 		arch_fence();
 
 		/* @todo: Print "Hello, world! (Core <core id>)" */
+		HELLO((int)cpuid());
+		/* @end todo */
 	}
 
 	set_return_addr(idle_entry);
