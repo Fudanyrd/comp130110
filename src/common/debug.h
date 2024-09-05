04dfbff88588560956c6448060370cc0ebe23e5c
Fudanyrd <143480204+Fudanyrd@users.noreply.github.com>
2024-09-05 08:13:58 +0800

    [X] format changed file

--- src/common/debug.h
+++ src/common/debug.h
@@ -7,15 +7,16 @@
 #ifdef ASSERT
 #undef ASSERT
 // assertion
-#define ASSERT(cond) \
-    do { \
-      if (!(cond)) { \
-        printk("Assertion failure: " #cond  \
-            " at [File %s] [Function %s] [Line %d]" \
-            , __FILE__, __FUNCTION__, __LINE__); \
-        for (;;) {} \
-      }\
-    } while(0)
+#define ASSERT(cond)                                        \
+    do {                                                    \
+        if (!(cond)) {                                      \
+            printk("Assertion failure: " #cond              \
+                   " at [File %s] [Function %s] [Line %d]", \
+                   __FILE__, __FUNCTION__, __LINE__);       \
+            for (;;) {                                      \
+            }                                               \
+        }                                                   \
+    } while (0)
 #endif
 
 #endif // __COMMON_DEBUG_
--- src/main.c
+++ src/main.c
@@ -7,54 +7,47 @@
 
 static volatile bool boot_secondary_cpus = false;
 
-#define HELLO(core) do { \
-		printk("Hello world! (Core %d)\n", core); \
-	} while(0)
+#define HELLO(core)                               \
+    do {                                          \
+        printk("Hello world! (Core %d)\n", core); \
+    } while (0)
 
 #ifdef ASSERT
 #undef ASSERT
 // assertion
-#define ASSERT(cond) \
-    do { \
-      if (!(cond)) { \
-        printk("Assertion failure: " #cond  \
-            " at [File %s] [Function %s] [Line %d]" \
-            , __FILE__, __FUNCTION__, __LINE__); \
-        for (;;) {} \
-      }\
-    } while(0)
+#define ASSERT(cond)                                        \
+    do {                                                    \
+        if (!(cond)) {                                      \
+            printk("Assertion failure: " #cond              \
+                   " at [File %s] [Function %s] [Line %d]", \
+                   __FILE__, __FUNCTION__, __LINE__);       \
+            for (;;) {                                      \
+            }                                               \
+        }                                                   \
+    } while (0)
 #endif
 
 void main()
 {
-<<<<<<< HEAD
+    /* edata is .bss start addr */
+    extern char edata[];
+    /* end is .bss end addr */
+    extern char end[];
+    ASSERT(end != NULL && edata != NULL);
+    ASSERT(end >= edata);
+
     if (cpuid() == 0) {
         /* @todo: Clear BSS section.*/
-=======
-	/* edata is .bss start addr */
-	extern char edata[];
-	/* end is .bss end addr */
-	extern char end[];
-	ASSERT (end != NULL && edata != NULL);
-	ASSERT (end >= edata);
-
-	if (cpuid() == 0) {
-		/* @todo: Clear BSS section.*/
-		memset(edata, 0, (usize)(end - edata));
-		/* @end todo */
->>>>>>> bc9c530cde6b65659e709c541a7f003f7271747e
+        memset(edata, 0, (usize)(end - edata));
+        /* @end todo */
 
         smp_init();
         uart_init();
         printk_init();
 
-<<<<<<< HEAD
         /* @todo: Print "Hello, world! (Core 0)" */
-=======
-		/* @todo: Print "Hello, world! (Core 0)" */
-		HELLO(0);
-		/* @end todo */
->>>>>>> bc9c530cde6b65659e709c541a7f003f7271747e
+        HELLO(0);
+        /* @end todo */
 
         arch_fence();
 
@@ -65,15 +58,10 @@ void main()
             ;
         arch_fence();
 
-<<<<<<< HEAD
         /* @todo: Print "Hello, world! (Core <core id>)" */
+        HELLO((int)cpuid());
+        /* @end todo */
     }
-=======
-		/* @todo: Print "Hello, world! (Core <core id>)" */
-		HELLO((int)cpuid());
-		/* @end todo */
-	}
->>>>>>> bc9c530cde6b65659e709c541a7f003f7271747e
 
     set_return_addr(idle_entry);
 }
