60226aa6a3ba4c0b4dbd5f25e0973e4a6c73f6ed
Fudanyrd <143480204+Fudanyrd@users.noreply.github.com>
2024-09-10 13:29:02 +0000

    [ENHANCE] use test_main to run tests
    - Also recover main.c

--- src/test_main.c
+++ src/test_main.c
@@ -1,11 +1,19 @@
 #include <aarch64/intrinsic.h>
 #include <driver/uart.h>
 #include <kernel/printk.h>
+#include <kernel/shutdown.h>
 #include <kernel/core.h>
 #include <common/string.h>
 #include <common/debug.h>
 #include <test/test_util.h>
 #include <test/test.h>
+
+/**
+ * A main.c that is specialized for building and running tests.
+ * 
+ * Difference from main.c: it runs tests and calls shutdown at 
+ * the end.
+ */
 
 static volatile bool boot_secondary_cpus = false;
 
@@ -35,11 +43,11 @@ void main()
     extern char edata[];
     /* end is .bss end addr */
     extern char end[];
-    ASSERT(end != NULL && edata != NULL);
-    ASSERT((void *)end >= (void *)edata);
 
     if (cpuid() == 0) {
         /* @todo: Clear BSS section.*/
+        ASSERT(end != NULL && edata != NULL);
+        ASSERT((void *)end >= (void *)edata);
         memset(edata, 0, (usize)(end - edata));
         /* @end todo */
 
@@ -47,24 +55,27 @@ void main()
         uart_init();
         printk_init();
         debug_init();
-
-        /* @todo: Print "Hello, world! (Core 0)" */
-        HELLO(0);
-        /* @end todo */
+        // boot shutdown module
+        shut_init();
+        shut_record();
 
         arch_fence();
 
         // Set a flag indicating that the secondary CPUs can start executing.
         boot_secondary_cpus = true;
+        // start running test
+        run_test();
     } else {
         while (!boot_secondary_cpus)
             ;
         arch_fence();
+        shut_record();
 
-        /* @todo: Print "Hello, world! (Core <core id>)" */
-        HELLO((int)cpuid());
-        /* @end todo */
+        // start running test
+        run_test();
     }
 
+    // shutdown the cpu, this will call 
+    shutdown();
     set_return_addr(idle_entry);
 }
