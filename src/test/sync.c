44ad1727e044bcb23249d2e6b598c37aecb07230
Fudanyrd <143480204+Fudanyrd@users.noreply.github.com>
2024-09-15 03:54:25 +0000

    [TEST] enable sync to work with any cpus

--- src/test/sync.c
+++ src/test/sync.c
@@ -1,28 +1,35 @@
 #include "sync.h"
 #include <aarch64/intrinsic.h>
 #include <common/spinlock.h>
+#include <common/rc.h>
+#include <aarch64/intrinsic.h>
 
 static SpinLock sync_lk;
 static volatile size_t counter;
+static volatile size_t ncpu;
 
 void sync_init(void)
 {
     counter = 0;
+    ncpu = 0;
     init_spinlock(&sync_lk);
+}
+
+/** Record number of cpus started */
+void sync_record()
+{
+    __atomic_fetch_add(&ncpu, 1, __ATOMIC_ACQ_REL);
 }
 
 void sync(size_t cnt)
 {
-    acquire_spinlock(&sync_lk);
-    counter++;
-    release_spinlock(&sync_lk);
+    arch_dsb_sy();
+    __atomic_fetch_add(&counter, 1, __ATOMIC_ACQ_REL);
 
     for (;;) {
-        acquire_spinlock(&sync_lk);
-        if (counter == cnt * 4) {
-            release_spinlock(&sync_lk);
+        if (counter == cnt * ncpu) {
             break;
         }
-        release_spinlock(&sync_lk);
     }
+    arch_dsb_sy();
 }
--- src/test/sync.h
+++ src/test/sync.h
@@ -1,6 +1,10 @@
 #pragma once
 /**
  * Sync.h: synchronize all threads(cpus).
+ * 
+ * Typical usage: call sync_init() in test_init(),
+ * and call sync_record() in run_test() and should
+ * be done BEFORE using sync functionality.
  */
 #ifndef __TEST_SYNC_
 #define __TEST_SYNC_
@@ -8,6 +12,7 @@
 #include <fdutil/stddef.h>
 
 void sync_init(void);
+void sync_record(void);
 
 /** Join all running threads */
 void sync(size_t cnt);
