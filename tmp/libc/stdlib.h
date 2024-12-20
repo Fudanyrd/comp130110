565296a3eb86ebfa42575d000b3db236019335e6
Fudanyrd <143480204+Fudanyrd@users.noreply.github.com>
2024-12-20 07:37:06 +0000

    [X] delete tmp's miscellaneous files

--- tmp/libc/stdlib.h
+++
@@ -1,7 +0,0 @@
-#ifndef __STRING_H_
-#define __STRING_H_ 1
-
-#include <stdint.h>
-extern void Exit(int code) __attribute__((noreturn));
-
-#endif // __STRING_H_
--- tmp/libc/string.h
+++
@@ -1,9 +0,0 @@
-#ifndef __STRING_H_
-#define __STRING_H_ 1
-
-#include <stdint.h>
-#include <stddef.h>
-extern uintptr_t Strlen(const char *s);
-extern void *memset(uint8_t *b, int v, size_t sz);
-
-#endif // __STRING_H_
